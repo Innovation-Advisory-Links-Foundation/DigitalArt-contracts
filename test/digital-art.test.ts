cec0290d02a423aad2fe85824b51977f752a8087
Jeeiii <giacomo.corrias7@gmail.com>
2021-06-16 16:50:02 +0200

    test: add tests for token mint features

--- test/digital-art.test.ts
+++ test/digital-art.test.ts
@@ -1,32 +1,99 @@
+import { Signer } from "@ethersproject/abstract-signer"
 import { Contract, ContractFactory } from "@ethersproject/contracts"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
 import { ethers } from "hardhat"
 
-describe("Dummy DigitalArt Test", () => {
-  it("Should mint a new NFT", async () => {
-    // DigitalArt contract deploy.
-    const DigitalArt: ContractFactory = await ethers.getContractFactory(
-      "DigitalArt"
-    )
-    const digitalArtIstance: Contract = await DigitalArt.deploy()
+// Digital Art Smart Contract unit tests.
+describe("*** DigitalArt ***", () => {
+  let DigitalArt: ContractFactory // Contract factory for istantiating new DigitalArt smart contract istances.
+  let digitalArtIstance: Contract // DigitalArt smart contract istance.
+  let signers: Array<SignerWithAddress> // Users accounts.
+  let artist: SignerWithAddress
+  let collector: SignerWithAddress
+
+  before(async () => {
+    // Get contract factory.
+    DigitalArt = await ethers.getContractFactory("DigitalArt")
+    // Create a new istance.
+    digitalArtIstance = await DigitalArt.deploy()
     await digitalArtIstance.deployed()
 
-    // Get tx signer and NFT receiver (owner).
-    const [signer, receiver]: Array<SignerWithAddress> =
-      await ethers.getSigners()
+    // Get users (signers) accounts.
+    signers = await ethers.getSigners()
+    artist = signers[0]
+    collector = signers[1]
+  })
 
-    // Send tx.
-    await digitalArtIstance
-      .connect(signer)
-      .safeMint(receiver.address, "dummyTokenURI")
+  describe("# NFT Minting", () => {
+    // NFT info.
+    const expectedTokenId = 1
+    const tokenURI = "ipfs://test/metadata.json"
+    const sellingPrice = 1000000
+    const dailyLicensePrice = 1000000
 
-    // Checks.
-    expect(
-      await digitalArtIstance.connect(signer).balanceOf(receiver.address)
-    ).to.be.equal(1)
-    expect(await digitalArtIstance.connect(signer).ownerOf(1)).to.be.equal(
-      receiver.address
-    )
+    it("Should be not possible to mint a token with an invalid selling price", async () => {
+      // Send tx.
+      const tx = digitalArtIstance
+        .connect(artist)
+        .safeMint(tokenURI, 0, dailyLicensePrice)
+
+      await expect(tx).to.be.reverted
+    })
+
+    it("Should be not possible to mint a token with an invalid daily license price", async () => {
+      // Send tx.
+      const tx = digitalArtIstance
+        .connect(artist)
+        .safeMint(tokenURI, sellingPrice, 0)
+
+      await expect(tx).to.be.reverted
+    })
+
+    it("Should mint a new NFT", async () => {
+      // Send tx.
+      const tx = await digitalArtIstance
+        .connect(artist)
+        .safeMint(tokenURI, sellingPrice, dailyLicensePrice)
+
+      // Wait until the tx is mined to get back the events.
+      const { events } = await tx.wait()
+      const { from, to, tokenId } = events[0].args
+
+      // Event checks.
+      expect(Number(from)).to.be.equal(0)
+      expect(to).to.be.equal(artist.address)
+      expect(Number(tokenId.toString())).to.be.equal(expectedTokenId)
+
+      // Storage checks.
+      const nft = await digitalArtIstance.idToNFT(expectedTokenId)
+      expect(nft.id).to.be.equal(expectedTokenId)
+      expect(nft.sellingPrice).to.be.equal(sellingPrice)
+      expect(nft.dailyLicensePrice).to.be.equal(dailyLicensePrice)
+      expect(nft.uri).to.be.equal(tokenURI)
+      expect(nft.artist).to.be.equal(artist.address)
+      expect(nft.owner).to.be.equal(artist.address)
+
+      // Methods checks.
+      expect(await digitalArtIstance.balanceOf(artist.address)).to.be.equal(
+        expectedTokenId
+      )
+      expect(await digitalArtIstance.ownerOf(expectedTokenId)).to.be.equal(
+        artist.address
+      )
+      expect(
+        await digitalArtIstance.getAllTokenIds(artist.address)
+      ).to.be.deep.equal([tokenId])
+      expect(await digitalArtIstance.tokenURI(tokenId)).to.be.equal(tokenURI)
+    })
+
+    it("Should be not possible to mint a token with the same IPFS CID twice", async () => {
+      // Send tx.
+      const tx = digitalArtIstance
+        .connect(artist)
+        .safeMint(tokenURI, sellingPrice, dailyLicensePrice)
+
+      await expect(tx).to.be.reverted
+    })
   })
 })
