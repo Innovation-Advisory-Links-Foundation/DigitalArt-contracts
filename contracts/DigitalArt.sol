adf29703398cc5005a9ef647ed41fd272065c40b
Jeeiii <giacomo.corrias7@gmail.com>
2021-06-16 15:16:17 +0200

    feat(digitalart): update safeMint method with new custom types for nft and licenses

--- contracts/DigitalArt.sol
+++ contracts/DigitalArt.sol
@@ -8,30 +8,136 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 /**
  * @title DigitalArt
  * @author Giacomo Corrias (@Jeeiii) - LINKS Foundation x OTB
- * @dev To do.
+ * @dev NFT marketplace w/ royalty redistribution and time-based purchases (licenses) for NFTs.
  */
 contract DigitalArt is ERC721URIStorage {
+    using Counters for Counters.Counter;
 
-    using Counters for Counters.Counter;   
-    // Private counter for NFT identifiers.
+    /** EVENTS */
+    // Price change when reselling.
+
+    /** CUSTOM TYPES */
+
+    struct NFT {
+        uint256 id; // Token unique identifier.
+        uint256 sellingPrice; // Selling price in wei (1 ETH == 10^18 wei).
+        uint256 dailyLicensePrice; // Daily license price in wei (1 ETH == 10^18 wei).
+        string uri; // An IPFS URI referencing a file containing the token metadata.
+        address payable artist; // Who created the NFT.
+        address payable owner; // Who owns the NFT.
+        bool onSale; // True when the NFT can be bought from the marketplace.
+        bool licensable; // True when someone can buy a time-based license for temporary usage.
+    }
+
+    struct License {
+        uint256 tokenId; // NFT unique identifier.
+        uint256 start; // Start date (in millis).
+        uint256 end; // End date (in millis)
+        uint256 price; // Price in wei (1 ETH == 10^18 wei).
+        address recipient; // Who bought the license.
+    }
+
+    /** STORAGE */
+
+    ///@dev Return the licenses belonging to the corresponding NFT.
+    mapping(uint256 => License[]) public tokenToLicenses;
+
+    ///@dev Return the NFT having the provided identifier.
+    mapping(uint256 => NFT) public _idToNFT;
+
+    ///@dev Private counter for NFT identifiers.
     Counters.Counter private _tokenIds;
-    
+
+    ///@dev Return the token id (NFT) associated to the corresponding IPFS URI.
+    mapping(string => uint256) private _uriToId;
+
+    ///@dev Return the token ids (NFTs) belonging to the corresponding address.
+    mapping(address => uint256[]) private _ownerToIds;
+
+    ///@dev Return the licenses belonging to the corresponding address.
+    mapping(address => License[]) private _userToLicenses;
+
     constructor() ERC721("DigitalArt", "DAT") {}
 
     /**
-     * @dev To do.
-     * @param _owner <address> - To do.
-     * @param _tokenURI <string> - To do.
-     * @return _newTokenId <uint256> - To do.
-     */ 
-    function safeMint (address _owner, string memory _tokenURI) external returns(uint256 _newTokenId) {
-        _tokenIds.increment();
-        
+     * @notice A safe method for minting a new NFT.
+     * @dev Stores on-chain the URI for the token metadata.
+     * @param _tokenURI <string> - An IPFS URI referencing a file containing the token metadata.
+     * @param _sellingPrice <uint256> - Selling price in wei (1 ETH == 10^18 wei).
+     * @param _dailyLicensePrice <uint256> - Daily license price in wei (1 ETH == 10^18 wei).
+     * @param _onSale <bool> - True when the NFT can be bought from the marketplace.
+     * @param _licensable <bool> - True when someone can buy a time-based license for temporary usage.
+     * @return _newTokenId <uint256> - Unique identifier of the NFT.
+     */
+    function safeMint(
+        string memory _tokenURI,
+        uint256 _sellingPrice,
+        uint256 _dailyLicensePrice,
+        bool _onSale,
+        bool _licensable
+    ) external returns (uint256 _newTokenId) {
+        require(_uriToId[_tokenURI] == 0, "ALREADY-MINTED");
+        require(
+            (_sellingPrice == 0 wei && _onSale == false) ||
+                (_sellingPrice > 0 wei && _onSale == true),
+            "INVALID-SELLING-PRICE"
+        );
+        require(
+            (_dailyLicensePrice == 0 wei && _licensable == false) ||
+                (_dailyLicensePrice > 0 wei && _licensable == true),
+            "INVALID-LICENSE-PRICE"
+        );
+
         // Mint a new token.
+        _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        _safeMint(_owner, newTokenId);
+        _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
-        
+
+        // NFT store.
+        NFT memory nft =
+            NFT(
+                newTokenId,
+                _sellingPrice,
+                _dailyLicensePrice,
+                _tokenURI,
+                payable(msg.sender),
+                payable(msg.sender),
+                _onSale,
+                _licensable
+            );
+
+        // Storage update.
+        _ownerToIds[msg.sender].push(newTokenId);
+        _uriToId[_tokenURI] = newTokenId;
+        _idToNFT[newTokenId] = nft;
+
         return newTokenId;
+    }
+
+    /**
+     * @notice Return the NFT ids owned by the address.
+     * @param _owner <address> - Address of the NFTs owner.
+     * @return _ids <uint256[]> - Unique identifiers of the NFTs owned by the address.
+     */
+    function getAllTokenIds(address _owner)
+        external
+        view
+        returns (uint256[] memory _ids)
+    {
+        return _ownerToIds[_owner];
+    }
+
+    /**
+     * @notice Return the licenses owned by the address.
+     * @param _owner <address> - Address of the licenses owner.
+     * @return _licenses <License[]> - Unique identifiers of the licenses owned by the address.
+     */
+    function getAllLicenses(address _owner)
+        external
+        view
+        returns (License[] memory _licenses)
+    {
+        return _userToLicenses[_owner];
     }
 }
