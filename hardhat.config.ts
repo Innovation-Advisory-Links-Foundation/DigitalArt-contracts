813bac6eec2ef9f53d165760140dccd761755112
Jeeiii <giacomo.corrias7@gmail.com>
2021-07-21 14:20:56 +0200

    chore: update hardhat config for working with ropsten testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+require("dotenv").config()
 import { config } from "./package.json"
 import { HardhatUserConfig } from "hardhat/config"
 
@@ -18,6 +19,10 @@ const hardhatConfig: HardhatUserConfig = {
         local: {
             url: "http://127.0.0.1:8545",
             gasPrice: 300000
+        },
+        ropsten: {
+            url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            accounts: [`0x${process.env.MARKETPLACE_OWNER_PRIVATE_KEY}`],
         }
     }
 }
